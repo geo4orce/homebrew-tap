7852f319f1245f984677f9429cddbc39f1bae9ce
Geo <geo@web-opt.com>
2026-08-27 19:02:53 -0400

    Publish vv 5.0.0

--- Formula/vv.rb
+++ Formula/vv.rb
@@ -1,8 +1,8 @@
 class Vv < Formula
   desc "Print any tool's version as a strict SemVer triple"
   homepage "https://verified-version.org/"
-  url "https://github.com/geo4orce/verified-version/archive/refs/tags/v4.1.0.tar.gz"
-  sha256 "6db18eea64564f350d0f941983b41dd6c76a96ac4a2a4f842ed7eecffebbe888"
+  url "https://github.com/geo4orce/verified-version/archive/refs/tags/v5.0.0.tar.gz"
+  sha256 "ff77c436c0ea1ada626f4f8fb8beea219bf28ca0d23214bcb1159ba444955e2b"
   license "MIT"
 
   def install
@@ -14,6 +14,6 @@ class Vv < Formula
   end
 
   test do
-    assert_equal "4.1.0", shell_output("#{bin}/vv --verified-version").strip
+    assert_equal "5.0.0", shell_output("#{bin}/vv --verified-version").strip
   end
 end
